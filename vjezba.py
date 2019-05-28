e3fbc1474746d04283b9289d1222ae271bf92428
GoranOr <goran.orsolic19@gmail.com>
2019-05-28 18:17:54 +0200

    Add files via upload

---
+++ vjezba.py
@@ -0,0 +1,36 @@
+#vjezba for
+
+#for i in range(1, 11):
+    #if i != 7:
+        #print(i)
+
+#broj = int(input("Unesi jedan broj:"))
+
+#for i in range(1, broj + 1):
+
+    #print(i)
+
+#primjer funkcije
+
+def pozdravi():
+
+    print("Dobar dan!")
+
+pozdravi()
+
+def say_hello(ime):
+
+    print("Heloo " + ime)
+
+say_hello("Ivan")
+
+say_hello("Ana")
+
+def moja_funkcija(x):
+
+    return 5 * x
+
+print(moja_funkcija(3))
+print(moja_funkcija(8))
+
+
